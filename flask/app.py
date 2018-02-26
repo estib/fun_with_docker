82326d93a627c722049c888a4ae23b9c451fd40d
szaporta <sarah.zaporta@datadoghq.com>
2018-02-26 13:32:22 -0500

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -5,5 +5,6 @@ app = Flask(__name__)
 def hello_world():
     return 'Huzzah! Flask has been Dockerized!!!'
 
+#Navigate to http://localhost:5000 to see if it works! :)
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
